7d942cee14e253040864cd5b581b6cc5f5a3cc04
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-02-10 15:09:48 +0800

    Update scTEAM-seq_debarcode.sh

--- scTEAM-seq_debarcode.sh
+++ scTEAM-seq_debarcode.sh
@@ -8,7 +8,7 @@ bash pipe.sh raw_sc_data
 for i in  `cat barcode_name.txt`
 do
 zcat raw_sc_data_out/*.csv.gz | grep $i >$i'.csv'
-/ifswh1/BC_RD/RD_COM/USER/chenweitian/software/seqtk-master/seqtk subseq raw_sc_data_out/raw_sc_data__clean.fastq.gz $i'.csv' |gzip > $i'.fq.gz'
+/ifswh1/BC_RD/RD_COM/USER/chenweitian/software/seqtk-master/seqtk subseq raw_sc_data_out/raw_sc_data_clean.fastq.gz $i'.csv' |gzip > $i'.fq.gz'
 
 done
 
