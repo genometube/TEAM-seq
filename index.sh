48be76d9c77086a12e1a2b5150b1b20be0dd1d26
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-02-10 14:24:23 +0800

    Add files via upload

---
+++ index.sh
@@ -0,0 +1,4 @@
+#/share/app/bowtie2-2.2.5/bowtie2-build /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_5_protease_Tn5/5_protease_Tn5_all.fa /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_5_protease_Tn5/5_protease_Tn5_all.fa
+
+#/share/app/bowtie2-2.2.5/bowtie2-build /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_6_Tn5/6_Tn5_all.fa /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_6_Tn5/6_Tn5_all.fa
+/share/app/bowtie2-2.2.5/bowtie2-build /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_hela_293/hela_293_barcode.fa /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_hela_293/hela_293_barcode.fa
