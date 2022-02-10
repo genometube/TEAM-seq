abb16c5b87018c69884d0287e20146cbf129dc95
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-02-10 15:22:34 +0800

    Update get_CGI_aver.coverage.depth.sh

--- get_CGI_aver.coverage.depth.sh
+++ get_CGI_aver.coverage.depth.sh
@@ -5,9 +5,9 @@ samtools bedcov  CpGisland_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_ch
 samtools bedcov  CpGisland_up4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/A1/alignment/phat/add_control/A1_total.bam >A1_total_up4kb.depth
 samtools bedcov CpGisland_down4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/A1/alignment/phat/add_control/A1_total.bam >A1_total_down4kb.depth
 
-python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_CGI_window_aver.depth.py A1_total_CpG.depth A1_total_CpG.aver.depth 20
-python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_CGI_window_aver.depth.py A1_total_up4kb.depth A1_total_up4kb.aver.depth 50
-python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_CGI_window_aver.depth.py A1_total_down4kb.depth A1_total_down4kb.aver.depth 50
+python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_CGI_window_aver.cov.depth.py A1_total_CpG.depth A1_total_CpG.aver.depth 20
+python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_CGI_window_aver.cov.depth.py A1_total_up4kb.depth A1_total_up4kb.aver.depth 50
+python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_CGI_window_aver.cov.depth.py A1_total_down4kb.depth A1_total_down4kb.aver.depth 50
 
 
 
