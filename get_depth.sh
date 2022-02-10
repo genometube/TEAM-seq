1c87b983b23f6a75446b706176c595fa7835ba67
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-02-10 11:09:19 +0800

    Update get_depth.sh

--- get_depth.sh
+++ get_depth.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 
 
-#samtools bedcov  CpGisland_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/A1/alignment/phat/add_control/A1_total.bam >A1_total_CpG.depth 
+samtools bedcov  CpGisland_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/A1/alignment/phat/add_control/A1_total.bam >A1_total_CpG.depth 
+samtools bedcov  CpGisland_up4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/A1/alignment/phat/add_control/A1_total.bam >A1_total_up4kb.depth
+samtools bedcov CpGisland_down4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/A1/alignment/phat/add_control/A1_total.bam >A1_total_down4kb.depth
 
-#samtools bedcov  CpGisland_up4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/A1/alignment/phat/add_control/A1_total.bam >A1_total_up4kb.depth
-
-#samtools bedcov CpGisland_down4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/A1/alignment/phat/add_control/A1_total.bam >A1_total_down4kb.depth
-
-
-
-samtools bedcov  CpGisland_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/3B/pipeline/alignment/B2/pbat/add_control/B2_total.bam >B2_total_CpG.depth
-
-
-samtools bedcov  CpGisland_up4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/3B/pipeline/alignment/B2/pbat/add_control/B2_total.bam >B2_total_up4kb.depth
+python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_window_depth_20.py A1_total_CpG.depth A1_total_CpG.aver.depth 
+python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_window_depth_50.py A1_total_up4kb.depth A1_total_up4kb.aver.depth
+python /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/compare/CpG/coverage_depth/get_window_depth_50.py A1_total_down4kb.depth A1_total_down4kb.aver.depth
 
 
 
-samtools bedcov CpGisland_down4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/3B/pipeline/alignment/B2/pbat/add_control/B2_total.bam  >B2_total_down4kb.depth
 
 
-samtools bedcov  CpGisland_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/3B/pipeline/alignment/test/alignment/B3/add_control/B3_total.bam >B3_total_CpG.depth
-
-
-samtools bedcov CpGisland_up4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/3B/pipeline/alignment/test/alignment/B3/add_control/B3_total.bam >B3_total_up4kb.depth
-
-
-samtools bedcov CpGisland_down4kb_window.txt /zfssz4/BC_RD_P1/PROJECT/P19Z12200N0088_chenwenfang/P19Z12200N0088_chenwenfang/LIANTI_Methy/3B/pipeline/alignment/test/alignment/B3/add_control/B3_total.bam >B3_total_down4kb.depth
 
 
