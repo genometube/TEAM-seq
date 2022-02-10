56668366d06142284a176044c8c1d3a24e3e6da2
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-02-10 11:12:03 +0800

    Add files via upload

---
+++ get_window_depth_50.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+
+'''
+input----- all cpg island franking region 50 window depth
+out------- 50 window average depth
+
+'''
+
+
+import sys
+
+out=open(sys.argv[2],'w')
+win={}
+
+for i in range(50):
+	key=i+1
+	win.setdefault(key,[0,0])
+
+
+#print (win)
+time=0
+for line in open(sys.argv[1],'r'):
+	aa=line[:-1].split('\t')
+	num=int(aa[3])
+	if num in win:
+		win[num][0]=win[num][0]+int(aa[-1])/80
+		win[num][1]=win[num][1]+1
+	
+	else:
+		continue
+
+for key in win:
+	out.write('\t'.join([str(key),str(win[key][0]),str(win[key][1]),str(win[key][0]/win[key][1])])+'\n')
+
+#	print(key,win[key]/time)	
+
+
+out.close()
