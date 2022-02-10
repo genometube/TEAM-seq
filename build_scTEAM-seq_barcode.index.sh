f1f620bbb90375a359a317826fe910318982b920
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-02-10 14:47:55 +0800

    Update build_scTEAM-seq_barcode.index.sh

--- build_scTEAM-seq_barcode.index.sh
+++ build_scTEAM-seq_barcode.index.sh
@@ -3,3 +3,6 @@
 awk '{print ">"$1"\n""NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNGGGAGA"$2 $3"AAATATATATAAAAAACAANNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"}' hela_293T_barcode.txt >hela_293_barcode.fa
 
 /share/app/bowtie2-2.2.5/bowtie2-build /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_hela_293/hela_293_barcode.fa /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_hela_293/hela_293_barcode.fa
+
+
+
