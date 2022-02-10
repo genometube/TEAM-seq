5d56ddd4ccb796b81085947b703db985433e1774
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-02-10 14:44:41 +0800

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -19,6 +19,7 @@ rm "$1_1.fastq.gz" "$1_11.fastq.gz" "$1_1r.fastq.gz" "$1_2r.fastq.gz" "$1_22r.fa
 echo ========== Cut P5/P7 Adapter of "$1" is done ==========
 
 ## Demultiplexing
+
 zcat "$1_cutP5P7.fastq.gz" |perl -nlE 'if(/runid/){say $_}else{if((length $_)>100){say substr($_,0,50).substr($_,-50)} else{say $_}}' |gzip > "$1_100.fastq.gz" &&\
 /ifswh1/BC_PUB/biosoft/pipeline/RNA/RNA_RNAref/RNA_RNAref_2018a/software/bowtie2/bowtie2 -x /zfswh1/BC_RD_P0/P19Z12200N0095_chenwenfang/chenwenfang/Project/sc_LIANTI/bowtie2_hela_293/hela_293_barcode.fa -U "$1_100.fastq.gz" -S "$1.sam" --local --np 0 --mp 1,0 --rdg 0,1 --rfg 0,1 --score-min L,52,0 2>debarcode.rate &&\
 /zfswh1/BC_RD_P0/zhangchen3/software/samtools/bin/samtools view -F4 -f16 "$1.sam" |cut -f1,3 |gzip > "$1_for.csv.gz" &&\
@@ -27,6 +28,7 @@ rm "$1_100.fastq.gz" "$1.sam" &&\
 echo ========== Demultiplexing of "$1" is done ==========
 
 ## Data cleaning
+
 /ifswh1/BC_PS/fangyitong/softwares/fastp -i "$1_cutP5P7.fastq.gz" -o "$1_1.fastq.gz" --adapter_sequence CTATCTCTTATACACAT --disable_quality_filtering &&\
 /ifswh1/BC_PS/fangyitong/softwares/fastp -i "$1_1.fastq.gz" -o "$1_2.fastq.gz" --adapter_sequence TTGTTTTTTATATATATT --disable_quality_filtering &&\
 /ifswh1/BC_RD/RD_COM/USER/chenweitian/software/seqtk-master/seqtk seq -r "$1_2.fastq.gz" |gzip > "$1_2r.fastq.gz" &&\
