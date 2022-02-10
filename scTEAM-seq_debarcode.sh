31a5b8bd1079d6af1fc8eb1e1badf1c7a848356a
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-02-10 15:09:11 +0800

    Update and rename work.sh to scTEAM-seq_debarcode.sh

---
+++ scTEAM-seq_debarcode.sh
@@ -0,0 +1,14 @@
+#!/usr/bash
+
+###   debarcode and get clean data
+bash pipe.sh raw_sc_data
+
+
+###   split clean fastq to single cell fastq data
+for i in  `cat barcode_name.txt`
+do
+zcat raw_sc_data_out/*.csv.gz | grep $i >$i'.csv'
+/ifswh1/BC_RD/RD_COM/USER/chenweitian/software/seqtk-master/seqtk subseq raw_sc_data_out/raw_sc_data__clean.fastq.gz $i'.csv' |gzip > $i'.fq.gz'
+
+done
+
